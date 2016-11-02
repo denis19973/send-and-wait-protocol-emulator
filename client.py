442d287c5b82f19a949523a998c5b74329e0d6f3
denis19973 <oxingteam@gmail.com>
2016-11-02 17:38:19 +0200

    small fixes

--- client.py
+++ client.py
@@ -1,45 +1,59 @@
 import time
 from abc import ABCMeta, abstractmethod
-
 import client_configuration
 import models
 from udp_network import *
 
 
 class Client(metaclass=ABCMeta):
+
+
+    # Constructor. Initializes the client in a mode.
     def __init__(self, mode):
+        self.configuration = client_configuration
         self.mode = mode
 
+    # The main runner..where all the client running occurs (either sending or receiving).
     @abstractmethod
     def run(self):
         pass
 
+    # Creates a packet from the configuration, sequence number, packet type and other details.
     @abstractmethod
     def make_packet(self, packet_type):
         pass
 
+    # Initialize the Udp Server for listening.
     def initialize_udp_server(self, port):
         self.listen = UDP_network.create_server(port)
 
+    # Send a packet to the network emulator.
     def send_packet(self, packet):
         socket = UDP_network.create_socket()
-        UDP_network.send_packet(socket, packet, '', client_configuration.network_port)
+        UDP_network.send_packet(socket, packet, self.configuration.network_address, self.configuration.network_port)
 
+    # Sets the client configuration.
     def set_configuration(self, network_address, network_port, transmitter_address, transmitter_port, \
                           receiver_address, receiver_port, max_packet_to_send, window_size, max_timeout):
-        client_configuration.network_address = network_address
-        client_configuration.network_port = network_port
-        client_configuration.transmitter_adress = transmitter_address
-        client_configuration.transmitter_port = transmitter_port
-        client_configuration.receiver_address = receiver_address
-        client_configuration.receiver_port = receiver_port
-        client_configuration.max_packet_to_send = max_packet_to_send
-        client_configuration.window_size = window_size
-        client_configuration.max_timeout = max_timeout
-
+        self.configuration.network_address = network_address
+        self.configuration.network_port = network_port
+        self.configuration.transmitter_adress = transmitter_address
+        self.configuration.transmitter_port = transmitter_port
+        self.configuration.receiver_address = receiver_address
+        self.configuration.receiver_port = receiver_port
+        self.configuration.max_packet_to_send = max_packet_to_send
+        self.configuration.window_size = window_size
+        self.configuration.max_timeout = max_timeout
+    # Prints all configuration for the Client.
     def print_configuration(self):
-        print('Mode: {}, Network Emulator Address: {} : {}'.format(self.mode, client_configuration.network_address, \
-                                                                   client_configuration.network_port))
+        print('Mode: {}, Network Emulator Address: {} : {}'.format(self.mode, self.configuration.network_address, \
+                                                                   self.configuration.network_port))
+    # Return client's current mode (sender or receiver).
+    def get_mode(self):
+        return self.mode
+    # Set the client mode (sender or receiver).
+    def set_mode(self, mode):
+        self.mode = mode
 
 
 class Sender(Client):
@@ -49,11 +63,11 @@ class Sender(Client):
         self.packet_window = []
 
     def run(self):
-        self.initialize_udp_server(client_configuration.transmitter_port)
+        self.initialize_udp_server(self.configuration.transmitter_port)
         self.send_control_packet()
         self.packets_sent = 0
 
-        while self.packets_sent < client_configuration.max_packets_to_sent:
+        while self.packets_sent < self.configuration.max_packets_to_sent:
             self.generate_window_and_send()
             self.waiting_for_acks = True
             self.set_time_for_acks()
@@ -63,7 +77,7 @@ class Sender(Client):
                     self.set_timer_for_acks()
                     print('Window status: {}'.format(len(self.packet_window)))
 
-            self.packets_sent += client_configuration.window_size
+            self.packets_sent += self.configuration.window_size
             print('Sent packets {}'.format(self.packets_sent))
 
         self.send_of_transmission_packet()
@@ -82,15 +96,15 @@ class Sender(Client):
         self.send_packet(packet)
 
     def make_packet(self, packet_type):
-        return models.Packet_Utilities.make_packet(client_configuration.receiver_address, \
-                                                   client_configuration.receiver_port, \
-                                                   client_configuration.transmitter_address, \
-                                                   client_configuration.transmitter_port, \
+        return models.Packet_Utilities.make_packet(self.configuration.receiver_address, \
+                                                   self.configuration.receiver_port, \
+                                                   self.configuration.transmitter_address, \
+                                                   self.configuration.transmitter_port, \
                                                    packet_type, self.seq_num, \
-                                                   self.seq_num, client_configuration.window_size)
+                                                   self.seq_num, self.configuration.window_size)
 
     def generate_window_and_sent(self):
-        for i in range(1, client_configuration.window_size):
+        for i in range(1, self.configuration.window_size):
             packet = self.make_packet(2)
             self.packet_window.append(packet)
             self.send_packet(packet)
@@ -110,7 +124,7 @@ class Sender(Client):
         self.timer = True
         while self.timer:
             self.ack_timeout()
-            time.sleep(client_configuration.max_timeout)
+            time.sleep(self.configuration.max_timeout)
 
     def receive_acks(self):
         self.listen.settimeout(2)
--- udp_network.py
+++ udp_network.py
@@ -5,21 +5,22 @@ from models import Packet
 
 
 class UDP_network:
+    # Creates a UDP Socket that listens on a specified port.
     def create_server(port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind(('', port))
         return sock
-
+    # # Creates a UDP Socket that listens on a specified port.
     def create_socket():
         return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+    # Read a Packet from the UDP socket.
     def get_packet(socket):
         byte_data = socket.recv(1024)
         packet = pickle.loads(byte_data)
         return packet
 
+    # Send a Packet from the UDP socket to destination specified inside the Packet.
     def send_packet(socket, packet, destination_address='', destination_port=8888):
-
 
         if destination_address != '':
             packet.set_destination_address(destination_address)
